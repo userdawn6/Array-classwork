6ece4a1aff699336581f4027b55ec6d9633b7bdb
Osetohanme <129763480+Osetohanme@users.noreply.github.com>
2024-06-11 13:49:28 +0100

    Add files via upload

---
+++ ArrayToString.java
@@ -0,0 +1,14 @@
+public class ArrayToString {
+    public static String arrayToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i]);
+            if (i < array.length - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
